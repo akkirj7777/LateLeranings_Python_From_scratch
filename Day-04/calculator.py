3b9bbf31672780e874ddfb1e69d58e6f50b1e6c3
AkshayRJ <159404621+akkirj7777@users.noreply.github.com>
2025-07-29 03:33:46 +0530

    Add files via upload

---
+++ Day-04/calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter First Number:") )
+num2 = int(input("Enter Second Number:") )
+
+
+def addition():
+    add = num1 + num2
+    print(add)
+
+def sub():
+    s = num1 - num2
+    print(s)
+
+def mul():
+    m = num1 * num2
+    print(m)
+
+addition()
+sub()
+mul()
