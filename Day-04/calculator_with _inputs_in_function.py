a7f5167a309ed1317ec30b824250312bea0bd4ce
AkshayRJ <159404621+akkirj7777@users.noreply.github.com>
2025-07-29 04:11:12 +0530

    Add files via upload

---
+++ Day-04/calculator_with _inputs_in_function.py
@@ -0,0 +1,22 @@
+'''Functtions do following things
+        1.Take inputs.
+        2.Perform the required logic.
+        3.Return the output.
+
+'''
+
+def addition(num1,num2):        # Given inputs to the function
+    add = num1 + num2           # Perform the reqired logic
+    return add                  # Returns the output
+
+def sub(num1,num2):
+    s = num1 - num2
+    return s
+
+def mul(num1,num2):
+    m = num1 * num2
+    return m
+
+print(addition(5,10))
+print(sub(10,6))
+print(mul(7,5))
